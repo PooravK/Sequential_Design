bd50f10c72406e91c7a738c322fc356f8e3659bf
PooravK <boomster.work@gmail.com>
2025-08-20 21:09:10 +0530

    Completed SR latch and testbench

--- ffsandlatches/sr_latch.v
+++ ffsandlatches/sr_latch.v
@@ -6,9 +6,9 @@ module sr_latch(
     always @(in_r or in_s)begin
         q <= 1'b0;
 
-        if (in_r == 1)begin //reset to 0 if res = 1
+        if (in_r == 1)begin //reset agar in_r 1 hai
             q <= 1'b0;
-        end else if (in_s == 1)begin //set to 1 if set = 1
+        end else if (in_s == 1)begin //set agar in_s 1 hai
             q <= 1'b1;
         end
     end
